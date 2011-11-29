275759e79cfa46899b2f1dbee6fcc56318eee6fd
cuneytcalishkan <cuneytcalishkan@036b31c3-b261-d235-4b1c-775543a326e9>
2011-11-29 13:02:55 +0000

    start

---
+++ trunk/EP2400Project2/src/edu/kth/ep2400/gradient/Peer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kth.ep2400.gradient;
+
+import peersim.core.Node;
+
+/**
+ *
+ * @author Cuneyt Caliskan
+ */
+public class Peer {
+
+    private Node node;
+    private int timeStamp;
+
+    public Peer(Node node, int timeStamp) {
+        this.node = node;
+        this.timeStamp = timeStamp;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public int getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(int timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Peer)) {
+            return false;
+        }
+        Peer other = (Peer) obj;
+        if (node.getID() == other.getNode().getID()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) node.getID();
+    }
+}
---
+++ trunk/EP2400Project2/src/edu/kth/ep2400/gradient/UtilityComparator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kth.ep2400.gradient;
+
+import java.util.Comparator;
+import peersim.core.Node;
+
+/**
+ *
+ * @author Cuneyt Caliskan
+ */
+public class UtilityComparator implements Comparator<Peer> {
+
+    private final Node node;
+    private final int protocolId;
+
+    public UtilityComparator(Node node, int protocolId) {
+        this.node = node;
+        this.protocolId = protocolId;
+    }
+
+    @Override
+    public int compare(Peer o1, Peer o2) {
+
+        return Gradient.compare(o1, o2, node, protocolId);
+    }
+}
