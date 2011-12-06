831fb1ff3ae01d1aff8a51519c8792ce7c6d34f3
cuneytcalishkan <cuneytcalishkan@036b31c3-b261-d235-4b1c-775543a326e9>
2011-12-06 10:54:50 +0000

    Message update successful:)

---
+++ trunk/EP2400Project2/src/edu/kth/ep2400/gradient/UpdatePuller.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kth.ep2400.gradient;
+
+import peersim.core.Control;
+
+/**
+ *
+ * @author cuneyt
+ */
+public class UpdatePuller implements Control {
+
+    @Override
+    public boolean execute() {
+
+        
+
+
+
+        return false;
+    }
+}
