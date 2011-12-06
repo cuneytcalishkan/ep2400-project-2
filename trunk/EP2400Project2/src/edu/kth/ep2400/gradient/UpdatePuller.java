e05f1cb53d15941bd4e8b7da5a064093c08ca77b
cuneytcalishkan <cuneytcalishkan@036b31c3-b261-d235-4b1c-775543a326e9>
2011-12-06 13:33:22 +0000

    Message pulling works, in a way:)

--- trunk/EP2400Project2/src/edu/kth/ep2400/gradient/UpdatePuller.java
+++ trunk/EP2400Project2/src/edu/kth/ep2400/gradient/UpdatePuller.java
@@ -4,7 +4,12 @@
  */
 package edu.kth.ep2400.gradient;
 
+import java.util.ArrayList;
+import java.util.List;
+import peersim.config.Configuration;
+import peersim.core.CommonState;
 import peersim.core.Control;
+import peersim.core.Network;
 
 /**
  *
@@ -12,13 +17,29 @@ import peersim.core.Control;
  */
 public class UpdatePuller implements Control {
 
+    private static final String PAR_PROTOCOL = "protocol";
+    private final int pid;
+
+    public UpdatePuller(String prefix) {
+        pid = Configuration.getPid(prefix + "." + PAR_PROTOCOL);
+    }
+
     @Override
     public boolean execute() {
 
-        
 
-
-
+        int rn = CommonState.r.nextInt(Network.size());
+        Gradient3 g = (Gradient3) Network.get(rn).getProtocol(pid);
+        Peer leader = g.whoIsTheLeader(1);
+        if (leader == null) {
+            return false;
+        }
+        List<Message> messages = new ArrayList<Message>();
+        messages = g.pullMessage(CommonState.r.nextInt(rn * CommonState.getIntTime()), CommonState.r.nextInt(rn * CommonState.getIntTime()));
+        System.out.println(messages.size() + " messages pulled");
+        for (Message message : messages) {
+            System.out.println(message);
+        }
         return false;
     }
 }
